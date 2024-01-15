e026bd769fc7994d2b0e6eedcdcfbb2a24ba9253
AnByeongDo <155951416+AnByeongDo@users.noreply.github.com>
2024-01-15 11:01:14 +0900

    test

---
+++ board/src/main/java/com/study/board/service/BoardService.java
@@ -0,0 +1,69 @@
+package com.study.board.service;
+
+import com.study.board.entity.Board;
+import com.study.board.repository.BoardRepository;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.hibernate.id.factory.internal.SequenceGenerationTypeStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class BoardService {
+
+    @Autowired
+    private BoardRepository boardRepository;
+    //글 작성 처리
+    public void write(Board board){
+
+        boardRepository.save(board);
+
+    }
+    //글 작성 처리
+    public void write(Board board, MultipartFile file) throws Exception{
+
+        String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
+
+        UUID uuid = UUID.randomUUID();
+
+        String fileName = uuid + "_" + file.getOriginalFilename();
+
+        File saveFile = new File(projectPath, fileName);
+
+        file.transferTo(saveFile);
+
+        board.setFilename(fileName);
+        board.setFilepath("/files/"+ fileName);
+        boardRepository.save(board);
+
+
+    }
+    //개시글리스트처리
+    public Page<Board> boardList(Pageable pageable){
+
+        return boardRepository.findAll(pageable);
+
+    }
+
+    public Page<Board> boardSearchList(String searchKeyword, Pageable pageable){
+
+        return boardRepository.findByTitleContaining(searchKeyword, pageable);
+
+    }
+
+    public Board boardView(Integer id){
+
+        return boardRepository.findById(id).get();
+    }
+
+    public void boardDelete(Integer id){
+
+        boardRepository.deleteById(id);
+    }
+}
